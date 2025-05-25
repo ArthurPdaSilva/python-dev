42f93f658a996771b95772a4ddf0020f1c883dee
Arthur Pereira da Silva <arthurnpdasilva@gmail.com>
2025-05-25 15:34:09 -0300

    feat: iniciando aprendizado

---
+++ inicio/exemplo.py
@@ -0,0 +1,15 @@
+print("Iniciando estudos em Agentes IA")
+
+# Dois tipos de arquivos ao trabalhar com Python: .py e .ipynb
+# .py: arquivo de código Python puro
+# .ipynb: arquivo Jupyter Notebook, que permite executar células de código e visualizar resultados interativos
+# Jupyter Notebook é uma ferramenta poderosa para análise de dados, visualização e aprendizado de máquina
+# Instalar bibliotacas: pip install para global 
+# pip install dash==2.18.0, quando não tem versão específica
+# pip show: para ver as bibliotecas instaladas
+# pip list: para ver todas as bibliotecas instaladas
+# pip freeze: para ver todas as bibliotecas instaladas com versão
+# Para instalar uma biblioteca em um ambiente virtual: python -m venv nome_do_ambiente (.venv)
+# Para ativar o ambiente virtual no Windows: source .venv/Scripts/activate
+# Para salvar as dependências do projeto: pip freeze > requirements.txt
+# Para instalar as dependências do projeto: pip install -r requirements.txt
